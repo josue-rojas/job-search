00372817785082431e51f9832b02da62e44d0020
josue rojas <10749061+josue-rojas@users.noreply.github.com>
2024-08-14 14:15:47 -0400

    [FEAT] more options for linkedin (#3)
    * [FEAT] Cron
    
    * [FEAT] Add more options for linkedin scraping

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ The idea is that some job boards (linkedin) are filled with "promoted" jobs when
 - Run `npm install`
 - .... Run `npm run dev` runs the script as is
 - jobs might not show up (working on adding a front end piece) but you can view them on sqlite using the queries from `Queries.md`
+
+## Notes
+Url Structure
+(Note: these arent all the query params that may exist it's just the ones that we have documented. They are subject to change by LinkedIn)
+https://www.linkedin.com/jobs/search/? - baseurl 
+currentJobId=3997599109 - job id to show
+&f_E=2%2C3%2C4 - experience level, they are a list of numbers (uri encoded)
+&f_TPR=r86400 - date posted (time in seconds)
+&geoId=102490898 - id location 
+&keywords=software%20engineer - job search keywords (uri encoded)
+&origin=JOB_SEARCH_PAGE_JOB_FILTER - I think this is for tracking on linkedin side (assuming from the name), it doesnt affect search
--- server/scrape.ts
+++ server/scrape.ts
@@ -4,38 +4,48 @@ import { SourceFactory, SourceType, SourceFactoryOptions } from "./sources/Sourc
 const jobRepo = new JobsRepository();
 
 async function main(sourceType: SourceType, sourceOptions: SourceFactoryOptions) {
-  const source = SourceFactory.createSource(sourceType, sourceOptions);
-  const sourceName = source.getSourceName();
-
-  console.log(new Date().toLocaleDateString(), `Source: ${sourceName}`);
-
-  const fetchData = await source.fetch();
-  const mappedData = source.mapData(fetchData);
-
-  const latestDateJobFound = (await jobRepo.getLatestJobDate(sourceName)).toISOString();
-
-  // insert all jobs found
-  for await (let newJob of mappedData.data) {
-    try {
-      await jobRepo.insertJob(newJob, sourceName);
-    } catch (err) {
-      console.error('erorr with', newJob.link);
-      console.error(err);
+  try {
+    
+    const source = SourceFactory.createSource(sourceType, sourceOptions);
+    const sourceName = source.getSourceName();
+  
+    console.log(new Date().toLocaleDateString(), `Source: ${sourceName}`);
+  
+    const fetchData = await source.fetch();
+    const mappedData = source.mapData(fetchData);
+  
+    const latestDateJobFound = (await jobRepo.getLatestJobDate(sourceName)).toISOString();
+  
+    // insert all jobs found
+    for await (let newJob of mappedData.data) {
+      try {
+        await jobRepo.insertJob(newJob, sourceName);
+      } catch (err) {
+        console.error('erorr with', newJob.link);
+        console.error(err);
+      }
     }
+  
+    const newData = await jobRepo.getLatestJobs(latestDateJobFound);
+  
+    console.log('Last Job Time:', latestDateJobFound);
+    console.log(newData.map((n) => [n.title, n.company, n.link, n.datePosted] ));
+  
+    return newData;
+  } catch (e) {
+    console.error('ERROR', e, sourceType, sourceOptions);
   }
-
-  const newData = await jobRepo.getLatestJobs(latestDateJobFound);
-
-  console.log('Last Job Time:', latestDateJobFound);
-  console.log(newData.map((n) => [n.title, n.company, n.link, n.datePosted] ));
-
-
-  return newData
-
 }
 
 (async () => {
-  await main(SourceType.LinkedIn, { keyword: 'TypeScript', location: 'New York' });
-  await main(SourceType.LinkedIn, { keyword: 'JavaScript', location: 'New York' });
-  await main(SourceType.LinkedIn, { keyword: 'software engineer', location: 'New York' });
+  const defaultTypes = {
+    location: 'New York',
+    geoId: '102490898',
+    experienceLevels: ['2', '3', '4'],
+    datePosted: 86400,
+
+  }
+  await main(SourceType.LinkedIn, { ...defaultTypes, keyword: 'TypeScript' });
+  await main(SourceType.LinkedIn, { ...defaultTypes, keyword: 'JavaScript'});
+  await main(SourceType.LinkedIn, { ...defaultTypes, keyword: 'software engineer' });
 })();
